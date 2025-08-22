267253919557dd8eb9ddfcff2d48c1fe6cba9e0e
hodahodahodahoho <hodahodahodahoho@gmail.com>
2025-08-22 21:34:24 +0300

    Update vanilla-tilt.js

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -2,8 +2,7 @@ var VanillaTilt = (function () {
 'use strict';
 
 /**
- * Created by Sergiu Șandor (micku7zu) on 1/27/2017.
- * Original idea: https://github.com/gijsroge/tilt.js
+ * Original Created by Sergiu Șandor (micku7zu) on 1/27/2017.
  * Modified by ME( Elhessein Eresha)
  */
 
